5639cd6f8be5eece18a8455a7d5a005adec5ac3a
Evan <evanfujita@gmail.com>
2021-01-13 20:59:00 -0600

    commit tree 547b0ba31f22a24004460e36285e477a16b0dd97

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,29 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+
+  get '/articles/new' do
+
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles/new' do
+    binding.pry
+    @article = Article.new(params)
+    
+    redirect '/articles/:id'
+  end
+
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    attr_accessor :title, :content
+
+    @@all = []
+
+    def initialize(params)
+        @title = params[:title]
+        @content = params[:content]
+        self.save
+        @@all << self
+    end
+
+
 end
